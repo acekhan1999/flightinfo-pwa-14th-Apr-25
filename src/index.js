7247c5531bd986985fae52d76eecc932cd149bef
arshadTrev2023 <arshad@trevex.io>
2025-04-24 19:37:28 +0400

    hardcoded /custom.js directory link in index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
 // Workbox service worker registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/custom-sw.js')
+    navigator.serviceWorker.register('https://acekhan1999.github.io/flightinfo-pwa-14th-Apr-25/custom-sw.js')
       .then(reg => {
         console.log('Custom SW registered:', reg);
       })
